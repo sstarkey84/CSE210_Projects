0b2b77bad4cb9bf5ef5ce7f63aad2850793a4291
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-03-06 07:18:52 -0800

    Finished exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your fist name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
+
