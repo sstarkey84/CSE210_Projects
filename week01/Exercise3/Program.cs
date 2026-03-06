4451bb5cdc2da2b1dafadea8c7d3c8024680baba
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-03-06 07:23:32 -0800

    Updated

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
-
         Random randomGenerator = new Random();
-        magicNumber = randomGenerator.Next(1, 101);
+        int magicNumber = randomGenerator.Next(1, 101);
 
         int guess = -1;
 
-        while (guess != magicNumber);
+        while (guess != magicNumber)
 
         {Console.Write("What is your guess? ");
         guess = int.Parse(Console.ReadLine());
