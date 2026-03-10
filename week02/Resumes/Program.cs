45255b7fca12db7d7d070f4df339d5530ffe4c8d
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-03-10 07:42:46 -0700

    finalized

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -11,8 +11,6 @@ class Program
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        job1.Display();
-
         Job job2 = new Job();
         
         job2._company = "Apple";
@@ -20,6 +18,12 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        job2.Display();
+        Resume myResume = new Resume();
+
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
