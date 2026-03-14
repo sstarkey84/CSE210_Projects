232c9ec2e597420664d336995918ca8dcb6ea06e
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-03-14 14:17:22 -0700

    finished

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        
+        int choice = 0;
+        while (choice != 5)
+        {
+            Console.WriteLine("\nJournal Menu");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quite");
+
+            Console.Write("Choose an option: ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+
+                Console.Write("> ");
+                string response = Console.ReadLine();
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = response;
+                journal.AddEntry(entry);
+                
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename: ");
+                string file = Console.ReadLine();
+                journal.SaveToFile(file);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename: ");
+                string file = Console.ReadLine();
+                journal.LoadFromFile(file);
+            }
+        }
+
     }
 }
