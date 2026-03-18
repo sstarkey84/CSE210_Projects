e395565dfff965f163df9ba90d4167fd053bd7da
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-03-18 09:42:32 -0700

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,82 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction a = new Fraction();
+        Fraction b = new Fraction(5);
+        Fraction c = new Fraction(3, 4);
+        Fraction d = new Fraction(1, 3);
+
+        Console.WriteLine(a.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        Console.WriteLine(b.GetFractionString());
+        Console.WriteLine(b.GetDecimalValue());
+
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
+
+        Console.WriteLine(d.GetFractionString());
+        Console.WriteLine(d.GetDecimalValue());
+
+        
+    }
+}
+
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        numerator = wholeNumber;
+        denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        if (denominator == 0)
+        {
+            this.denominator = 1;
+        }
+        else
+        {
+            this.denominator = denominator;
+        }
+    }
+    public int GetTop()
+    {
+        return numerator;
+    }
+    public int GetBottom()
+    {
+        return denominator;
+    }
+    public void SetTop(int top)
+    {
+        numerator = top;
+    }
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            denominator = bottom;
+        }
+    }
+    public string GetFractionString()
+    {
+        return numerator + "/" + denominator;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
     }
 }
