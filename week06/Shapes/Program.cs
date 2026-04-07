bd28a771e08c596f622ccfc420e4f8971b949bdd
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-04-07 08:11:03 -0700

    Finished Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square square1 = new Square("Red", 5);
+        Rectangle rect1 = new Rectangle("Green", 6, 3);
+        Circle cir1 = new Circle("Yellow", 8);
+
+        shapes.Add(square1);
+        shapes.Add(rect1);
+        shapes.Add(cir1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} {s.GetType().Name} has an area of {area:F0}.");
+        }
     }
 }
