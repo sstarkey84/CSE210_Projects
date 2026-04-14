18991d4b5f8eec0ee489c2cbfd5ea25fd5969165
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-04-14 00:20:38 -0700

    Exercise Tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+        activities.Add(new Running("03 Nov 2022", 30, 3.0));
+        activities.Add(new Cycling("10 April 2023", 40, 29.5));
+        activities.Add(new Swimming("30 Mar 2025", 20, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
